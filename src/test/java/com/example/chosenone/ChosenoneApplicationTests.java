cecb3e1f8eef385c69c6ee46ee78ae3f4010d840
alemisiu <alek@siudek.net>
2026-06-30 23:05:00 +0200

    prepared fundamentals all in one

---
+++ src/test/java/com/example/chosenone/ChosenoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chosenone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChosenoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
