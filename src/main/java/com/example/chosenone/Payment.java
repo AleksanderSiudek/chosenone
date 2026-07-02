79bcb9adaaa77a072bff4cdda2a0c03b69c21e07
alemisiu <alek@siudek.net>
2026-07-02 15:52:01 +0200

    refactor Payment class: change id type to Long and update validation logic

--- src/main/java/com/example/chosenone/Payment.java
+++ src/main/java/com/example/chosenone/Payment.java
@@ -3,16 +3,16 @@ package com.example.chosenone;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record Payment(String idOfMember, BigDecimal amount, LocalDate data) {
-    public Payment { // <- bez nawiasów z parametrami! to konstruktor kompaktowy
-        if (idOfMember == null || idOfMember.isBlank()) {
+public record Payment(Long idOfMember, BigDecimal amount, LocalDate data) {
+    public Payment {
+        if (idOfMember == null || idOfMember < 0) {
             throw new IllegalArgumentException("Id of member cannot be null");
         }
         if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("amount cannot be null");
+            throw new IllegalArgumentException("Amount cannot be null");
         }
         if (data == null) {
-            throw new IllegalArgumentException("date cannot be null");
+            throw new IllegalArgumentException("Date cannot be null");
         }
     }
 }
