fe57c994285f9c622c3ceb50ebe07cba352ed29b
alemisiu <alek@siudek.net>
2026-07-02 15:51:52 +0200

    refactor Member class: change id type to Long and update validation logic

--- src/main/java/com/example/chosenone/Member.java
+++ src/main/java/com/example/chosenone/Member.java
@@ -3,24 +3,22 @@ package com.example.chosenone;
 import java.util.Objects;
 
 public class Member {
-    private final String id; // np. numer lokalu albo UUID
+    private final Long id; // np. numer lokalu albo UUID
     private final String imieNazwisko;
 
-    public Member(String id, String imieNazwisko) {
-       
-        if (id == null || id.isBlank()) {
+    public Member(Long id, String imieNazwisko) {
+
+        if (id == null || id < 0) {
             throw new IllegalArgumentException("Id cannot be empty or null");
         }
         if (imieNazwisko == null || imieNazwisko.isBlank()) {
-            throw new IllegalArgumentException("nameAndUsername cannot be empty or null");
+            throw new IllegalArgumentException("NameAndUsername cannot be empty or null");
         }
         this.id = id;
         this.imieNazwisko = imieNazwisko;
     }
 
- 
-
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
@@ -29,15 +27,18 @@ public class Member {
     }
 
     @Override
-    public boolean equals(Object o){
-        if(this == o) return true;
-        if(o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         var member = (Member) o;
         return Objects.equals(id, member.id);
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return Objects.hash(id);
     }
-   
+
 }
